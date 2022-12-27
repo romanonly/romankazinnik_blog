8b3629de5a91a6a5d2108388a1da6718e6de4dd1
Roman Kazinnik <roman.kazinnik@gmail.com>
2022-12-26 22:46:02 -0800

    lint python code
    lint code clean

--- active_learning/text_deduplication/create_docker.sh
+++ active_learning/text_deduplication/create_docker.sh
@@ -13,18 +13,18 @@ for i in $( ls ); do
 done
 
 # alias PWD=$("pwd -P")
-PWD=$( pwd -P)
+PWD=$( pwd -P )
 echo PWD=${PWD}
 
 
 echo  -e "\nInstructions:\n uncomment to run\n"
 
-echo  -e "\nStep-1:\n"
-echo docker run  -v ${PWD}/metrics:/usr/app/metrics  ${full_image_name} python3 -m metrics.run_blocks
+echo  -e "\nStep-1: param = ${1}\n"
+echo docker run  -v ${PWD}/metrics:/usr/app/metrics  ${full_image_name} python3 -m metrics.run_blocks ${1}
 
 echo  -e "\nStep-2: Intreactuive Labeling\n"
 echo "run in container run_clf: EXIT when done"
-echo docker run  -v ${PWD}/metrics:/usr/app/metrics -it ${full_image_name} /bin/bash
+docker run  -v ${PWD}/metrics:/usr/app/metrics -it ${full_image_name} /bin/bash
 echo python3 -m metrics.run_clf
 
 echo  -e "\nStep-3: create duplicate pairs\n"
@@ -32,5 +32,5 @@ echo docker run  -v ${PWD}/metrics:/usr/app/metrics  ${full_image_name} python3 
 
 echo  -e "\nStep-4: Calibrate and create two lists: unique and duplicate (use Step-3 duplicate pairs) \n"
 echo "run in container run_final: EXIT when done"
-docker run  -v ${PWD}/metrics:/usr/app/metrics -it ${full_image_name} /bin/bash
+echo docker run  -v ${PWD}/metrics:/usr/app/metrics -it ${full_image_name} /bin/bash
 echo python3 -mmetrics.run_final
---
+++ active_learning/text_deduplication/metrics/run_dedup.py
@@ -0,0 +1,129 @@
+import pandas as pd
+import pickle
+import logging
+import numpy as np
+import random
+import gc
+from multiprocessing import Pool
+# UserWarning: Loky-backed parallel loops cannot be called in a multiprocessing, setting n_jobs=1
+import warnings
+
+from metrics.run_clf import predict_proba, create_train_rows
+
+warnings.filterwarnings("ignore")
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+pd.options.mode.chained_assignment = None  # default='warn'
+
+pd.set_option("display.max_rows", 500)
+pd.set_option("display.max_columns", 500)
+pd.set_option("display.width", 1000)
+
+DEDUP_BATCH_SIZE = 200000
+DEDUP_PROB_THRESHOLD = 0.5
+NUM_RANDOM_BLOCKS_SELECT = -1
+BLOCKS_PER_PROCESS = 100
+
+PATH = "metrics"
+cols_save = [
+    "idx_x",
+    "idx_y",
+    "name_x",
+    "name_y",
+    "platform_x",
+    "platform_y",
+    "dist",
+    "probability_duplicate",
+]
+
+
+def write_duplicates(df_k_k_list: pd.DataFrame, fn1: str):
+    try:
+        df_duplicates = pd.DataFrame()
+        df_unlabeled_concat = pd.DataFrame()
+        # scale-score in large chunks
+        for ind, df_unlabeled_ind in enumerate(df_k_k_list):
+            if df_unlabeled_ind is not None and len(df_unlabeled_ind) > 0:
+                df_unlabeled_concat = pd.concat(
+                    objs=[df_unlabeled_concat, df_unlabeled_ind], axis=0
+                )
+            # Write Batch Inferences
+            if ind == len(df_k_k_list) - 1 or len(df_unlabeled_concat) > DEDUP_BATCH_SIZE:
+                # df_Unlabeled_concat has bool cols and df_a miust be float
+                df_a = df_unlabeled_concat.copy()
+                df_a[cols_pred] = df_a[cols_pred].astype(float)
+                df_a.loc[:, cols_pred] = scaler.transform(df_unlabeled_concat[cols_pred])
+                df_unlabeled_concat = pd.DataFrame()
+                # Scoring and Threshold at 0.5
+                pred_prob = predict_proba(clf, df_a, cols_pred)
+                pos_mask = pred_prob > DEDUP_PROB_THRESHOLD
+                pos_ind = np.where(pos_mask)[0]
+                df_a["probability_duplicate"] = pred_prob
+                df_pos = df_a.iloc[pos_ind]
+                if len(df_pos) > 0:
+                    df_pos.reset_index(drop=True, inplace=True)
+                    df_duplicates = pd.concat(
+                        objs=[df_duplicates, df_pos[cols_save]], axis=0
+                    )
+                    # logger.info(f"\n{df_pos[cols_save]}\n") # df_pos[cols_view]
+                    df_duplicates.drop_duplicates(
+                        ["idx_x", "idx_y"], keep="last", inplace=True
+                    )
+                    df_duplicates.to_csv(fn1)
+        df_duplicates.sort_values(
+            by=["probability_duplicate"], ascending=False, inplace=True
+        )
+        df_duplicates.reset_index(drop=True, inplace=True)
+        df_duplicates.to_csv(fn1)
+        logger.info(f"writing {fn1}")
+    except Exception as e:
+        logger.info(f"\nException occurred:{type(e)} (type), {e}\n")
+    logger.info(f"{fn1} done\n")
+
+
+def process_block(ind_block):
+    """ list of blocks each block is DataFrame"""
+    ind = ind_block[0]
+    df_k_list = ind_block[1]
+    logger.info(f"parallel process {ind} (chunks of {BLOCKS_PER_PROCESS} blocks)")
+    df_k_k_list = [create_train_rows(d) for d in df_k_list if d is not None]
+    write_duplicates(df_k_k_list, fn1=f"{PATH}/duplicates_big_{ind}.csv")
+    return ind
+
+
+def run_parallel(blocks):
+    blocks_li_li = [
+        blocks[i : i + BLOCKS_PER_PROCESS]
+        for i in range(0, len(blocks), BLOCKS_PER_PROCESS)
+    ]
+    logger.info(f"multiprocessing Pool: *** {len(blocks_li_li)} *** parallel lists")
+    pool = Pool()  # processes=NUM_THREADS)  # 8 cores/16
+    df_k_k_li_li = pool.map(
+        process_block, zip(range(len(blocks_li_li)), blocks_li_li)
+    )
+    # df_k_k_list = [i for sub in df_k_k_li_li for i in sub if item is not None]
+    logger.info(f"multiprocessing Pool finished: {len(df_k_k_li_li)} processes")
+    return df_k_k_li_li
+
+
+if __name__ == "__main__":
+    try:
+        logger.info(f"\n...open blocks for 500K records\n")
+        pickle_off = open(f"{PATH}/blocks.pickle", "rb")
+    except Exception as e:
+        logger.info(f"\n.../blocks.pickle not exsit {e}, open blocks for small subset 20K records\n")
+        pickle_off = open(f"{PATH}/blocks_small.pickle", "rb")
+
+    total_df_k_k_list, df_all, df_anom = pickle.load(pickle_off)
+    logger.info(f"reading {PATH}/clf.pickle")
+    pickle_off = open(f"{PATH}/clf.pickle", "rb")
+    clf, scaler, cols_pred, cols_view = pickle.load(pickle_off)
+    # create duplicates list
+    if NUM_RANDOM_BLOCKS_SELECT > 0:
+        blocks = random.sample(total_df_k_k_list, NUM_RANDOM_BLOCKS_SELECT)
+    else:
+        blocks = total_df_k_k_list
+    n = gc.collect()
+    logger.info(f"Unreachable objects collected by GC:{n}. un-collectable {gc.garbage}")
+    _ = run_parallel(blocks)
+    exit(1)
