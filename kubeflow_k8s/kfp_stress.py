f229904e20d130802229f09a3b8bc584de997ed2
Roman Kazinnik <roman.kazinnik@gmail.com>
2023-01-03 06:11:05 -0800

    minor

--- kubeflow_k8s/kfp_stress.py
+++ kubeflow_k8s/kfp_stress.py
@@ -7,10 +7,10 @@ import kfp
 
 package_path = 'pipeline_amazon.yaml'
 
+EXPERIMENT_NAME = 'stress_tests'
 
-def run_exp(params=None, pipeline_filename=None, pipeline_package_path=None, pipeline_func=None):
-    EXPERIMENT_NAME = 'stress_test'
 
+def run_exp(job_index='-run', params=None, pipeline_filename=None, pipeline_package_path=None, pipeline_func=None):
     if pipeline_filename is not None:
         assert pipeline_func is not None
         kfp.compiler.Compiler(mode = kfp.dsl.PipelineExecutionMode.V2_COMPATIBLE).compile(
@@ -26,20 +26,29 @@ def run_exp(params=None, pipeline_filename=None, pipeline_package_path=None, pip
 
     client.run_pipeline(
         experiment_id = experiment.id,
-        job_name = pipeline_filename + '-run',
+        job_name = pipeline_filename + job_index,
         pipeline_package_path = pipeline_filename,
         params = params
     )
 
 
-params = {'epochs': 1000, 'batch_size': 12, 'num_samples': -1, 'learning_rate': 1e-4}
+params = {'epochs': 100, 'batch_size': 32, 'num_samples': -1, 'learning_rate': 1e-4}
 
 # Stress serial jobs examples
 
-for batch_size in range(1, 100):
-    params['batch_size'] = 8 * batch_size
-    run_exp(params = params, pipeline_package_path = package_path)
+# Run network components
+initStress = True
 
-for learning_rate in [1e-1, 1e-2, 1e-3, 1e-4]:
-    params['learning_rate'] = learning_rate
-    run_exp(params = params, pipeline_package_path = package_path)
+if initStress:
+    for learning_rate in [1e-1, 1e-2, 1e-3, 1e-4]:
+        params['learning_rate'] = learning_rate
+        run_exp(params = params, pipeline_package_path = package_path)
+else:
+    # stress test
+    maxJobs = 20
+    params['learning_rate'] = 1e-3
+    params['num_samples'] = 1000
+
+    for ind, batch_size in enumerate(range(1, 1 + maxJobs)):
+        params['batch_size'] = 8 * batch_size
+        run_exp(job_index = f"-batch-index-{ind}", params = params, pipeline_package_path = package_path)
